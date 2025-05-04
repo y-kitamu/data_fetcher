2b52b79e4837d0898b3d8bc6abf3d6c9e826cf55
kitamura <ymyk6602@gmail.com>
2025-05-04 21:46:02 +0900

    add kabutan fetcher

--- data_fetcher/kabutan/__init__.py
+++ data_fetcher/kabutan/__init__.py
@@ -2,3 +2,13 @@
 
 from . import data, financial
 from .io import read_data_csv, read_financial_csv, write_data_csv
+from .kabutan_fetcher import KabutanFetcher
+
+__all__ = [
+    "KabutanFetcher",
+    "data",
+    "financial",
+    "read_data_csv",
+    "read_financial_csv",
+    "write_data_csv",
+]
