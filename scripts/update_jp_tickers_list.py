e2b81682251bebdca074161f707f2c3e82d40e57
kitamura <ymyk6602@gmail.com>
2025-06-16 19:33:04 +0900

    update script

--- scripts/update_jp_tickers_list.py
+++ scripts/update_jp_tickers_list.py
@@ -3,10 +3,12 @@
 """
 
 import csv
+import time
 from pathlib import Path
 
 import requests
 import xlrd
+from bs4 import BeautifulSoup
 
 import data_fetcher
 
@@ -30,8 +32,43 @@ def save_code_list_to_csv(
         csv_writer.writerows(rows)
 
 
+def extract_themes(html):
+    soup = BeautifulSoup(html)
+    kobetu_right = soup.find("div", attrs={"id": "kobetsu_right"})
+    if kobetu_right is None:
+        return []
+    company_block = kobetu_right.find("div", attrs={"class": "company_block"})
+    if company_block is None:
+        return []
+    themes = []
+    for trow in company_block.find_all("tr"):
+        header = trow.find("th", attrs={"scope": "row"})
+        if header is not None and header.text == "テーマ":
+            themes += [li.text for li in trow.find_all("li")]
+    return themes
+
+
+def update_themes_csv():
+    tickers = data_fetcher.ticker_list.get_jp_ticker_list()
+    base_url = "https://kabutan.jp/stock/?code={}"
+    ticker_themes = []
+    for ticker in tickers:
+        url = base_url.format(ticker)
+        response = requests.get(url)
+        ticker_themes.append([ticker, extract_themes(response.text)])
+        time.sleep(0.2)
+        # break
+
+    output_file = data_fetcher.constants.PROJECT_ROOT / "data/jp_ticker_themes.csv"
+    with open(output_file, "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["ticker", "themes"])
+        writer.writerows(ticker_themes)
+
+
 def main():
     save_code_list_to_csv(data_fetcher.constants.JP_TICKERS_PATH)
+    update_themes_csv()
 
 
 if __name__ == "__main__":
