52a78f48d909a84cacb6e3ea2cc68eaf4218f435
kitamura <ymyk6602@gmail.com>
2026-08-14 11:25:02 +0900

    ci to local

--- scripts/cron_fetch_from_kabutan_news.sh
+++ scripts/cron_fetch_from_kabutan_news.sh
@@ -3,7 +3,9 @@ PATH=/home/kitamura/.local/bin${PATH:+:${PATH}}
 
 cd /home/kitamura/work/data_fetcher
 echo "Start fetch_jp_news.py"
+uv run python scripts/update_jp_tickers_list.py
 uv run python scripts/fetch_jp_news.py --days 30
 uv run python scripts/fetch_data_from_kabutan.py
 uv run python scripts/update_financial_data_jp.py
+uv run python scripts/divide_stocks_jp.py
 echo "Finish fetch_jp_news.py"
